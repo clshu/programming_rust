c61f94e16ffde7eb65c3ef7784d417dcb1546d77
clshu <clshu2020@gmail.com>
2022-09-15 16:31:58 +0800

    ch05: Create fn show() and implement main().

--- ch05_references/src/main.rs
+++ ch05_references/src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
+type Table = HashMap<String, Vec<String>>;
+
+fn show(table: Table) {
+    for (artist, works) in table {
+        println!("Works by {}:", artist);
+        for work in works {
+            println!(" {}", work);
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut table = Table::new();
+    table.insert("Gesualdo".to_string(), vec![]);
+    table.insert(
+        "Caravaggio".to_string(),
+        vec![
+            "The Musicians".to_string(),
+            "The Calling of Saint Matthew".to_string(),
+        ],
+    );
+    table.insert(
+        "Cellini".to_string(),
+        vec![
+            "Perseus with the head of Medusa".to_string(),
+            "a salt cellar".to_string(),
+        ],
+    );
+    show(table);
 }
